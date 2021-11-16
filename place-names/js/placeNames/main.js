a0e64fe6af3f7aab2183f816bbb4430b3988fde9
joshuastewart417 <joshua.stewart780@gmail.com>
2021-11-16 10:21:54 -0600

    initial commit

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,24 @@
 // Put your code here
 
+const { Console } = require("console");
+
 const names = [
    "Nashville", "Hong Kong", "The back yard", "Earth",
     "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
 ];
 
-console.log("All Pace Names");
+console.log("All Place Names");
 
 for (const name of names) {
+  console.log(name);
+}
+
+console.log("")
+
+const theNames = names.filter(name => name.startsWith("The"))
+
+console.log("'The' Place Names");
+
+for(const name of theNames) {
   console.log(name);
 }
