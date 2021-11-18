84561b929a70a5f94387d36257e40aa377d028dd
joshuastewart417 <joshua.stewart780@gmail.com>
2021-11-18 10:09:40 -0600

    completed rolling dice js

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,11 +1,69 @@
+
+
 console.log("Let's roll some dice, baby!")
 console.log("---------------------------")
 
 
 
-function Roll() {
-    let dieValue = Math.floor((Math.random) * 6)
-    return dieValue
+function Roll(input) {
+    return {
+        rollNum: input,
+        rollString: numToString(input)
+    }
 }
 
-let die = Roll()
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function numToString(input) {
+
+    let dieString = ""
+
+    switch (input)
+        {
+           
+            case 1:
+                dieString = "one";
+                break;
+            case 2:
+                dieString = "two";
+                break;
+            case 3:
+                dieString = "three";
+                break;
+            case 4:
+                dieString = "four";
+                break;
+            case 5:
+                dieString = "five";
+                break;
+            case 6:
+                dieString = "six";
+                break;
+        }
+
+        return dieString;
+    }
+
+
+
+for (let i = 0; i < 10; i++) {
+
+    let die1 = Roll(getRandomInt(1, 6))
+    let die2 = Roll(getRandomInt(1, 6))
+
+    let message = `${die1.rollString} + ${die2.rollString} == ${die1.rollNum + die2.rollNum}`;
+    if (die1.rollNum == die2.rollNum)
+    {
+        message += " DOUBLES!";
+    }
+
+    console.log(message);
+
+}
+
+
+
